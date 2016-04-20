fba4def46fb4d3bde34e5009e4d0990b88c007b1
bhanuka <bhanukamax@gmail.com>
2016-04-21 01:59:42 +0530

    adding files

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var coffee = require('gulp-coffee');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+
+
+gulp.task('styles', function(){
+  gulp.src(['src/styles/**/*.scss'])
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('dist/styles/'))
+});
+
+gulp.task('scripts', function(){
+  return gulp.src('src/scripts/**/*.coffee')
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(coffee({bare: true})
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/scripts/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts/'))
+});
+
+gulp.task('default', function(){
+  gulp.watch("src/styles/**/*.scss", ['styles']);
+  gulp.watch("src/scripts/**/*.coffee", ['scripts']);
+});
---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+//@prepros-prepend jquery-1.12.2.js
+//@prepros-prepend bootstrap.min.js
+//@prepros-prepend main.js
